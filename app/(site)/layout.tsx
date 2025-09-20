a83d19fa05c8233759fc10d7f8ed399eb90a835e
Rishang <rishangkumar9693@gmail.com>
2025-09-20 17:36:25 +0530

    feat: add Header component and navigation menu
    - Created a new Header component for the landing page, which includes navigation links and buttons for login and signup.
    - Integrated the Header component into the HomePage layout.
    - Implemented a NavigationMenu component using Radix UI for better navigation structure.
    - Added new routes and components for the navigation menu.
    - Updated package.json and package-lock.json to include new dependencies for navigation menu and authentication.
    - Modified Prisma schema to include Account and Session models for user authentication.
    - Added migration SQL for creating Account and Session tables with foreign key constraints.

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,10 +1,13 @@
-
-import React from 'react'
+import Header from "@/components/landing-page/header";
+import React from "react";
 
 const HomePagelayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main>{children}</main>
-  )
-}
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
+};
 
 export default HomePagelayout;
