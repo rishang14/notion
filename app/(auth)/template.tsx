d88d046de897dc829d80993f2b88f74ec52137e2
Rishang <rishangkumar9693@gmail.com>
2025-09-20 15:09:11 +0530

    feat: implement authentication pages with login and signup forms; add form components and validation schema

---
+++ app/(auth)/template.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface TemplateProps {
+  children: React.ReactNode;
+}
+
+const Template= ({ children }:TemplateProps) => {
+  return (
+    <div
+      className="
+      h-screen
+      p-6 flex 
+      justify-center"
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Template;
